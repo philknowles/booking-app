25ed5542e31c8f72f646450d5f5ac85a28096a3b
Phil Knowles <fighters4christ@outlook.com>
2022-05-09 09:42:01 -0500

    Saving progress

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
-import React from './home.css';
+import './home.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
+import Featured from '../../components/featured/Featured';
+import PropertyList from '../../components/propertyList/propertyList';
+import FeaturedProperties from '../../components/featuredProperties/featuredProperties';
 
 const Home = () => {
   return (
@@ -8,7 +11,15 @@ const Home = () => {
       <Navbar />
       <Header />
       <div className="homeContainer">
-        
+        <Featured />
+        <h1 className="homeTitle">
+          Browse by Property Type
+        </h1>
+        <PropertyList />
+        <h1 className="homeTitle">
+          Homes Guests Love
+        </h1>
+        <FeaturedProperties />
       </div>
     </div>
   )
